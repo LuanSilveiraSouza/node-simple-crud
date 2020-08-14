09f3549437ad0dd778c3199be95e2340937717de
LuanSouzaSilveira <luansouzasilveira@gmail.com>
2020-08-14 10:57:32 -0300

    Update route

--- src/controller/MangaController.js
+++ src/controller/MangaController.js
@@ -1,6 +1,17 @@
 const db = require('../database');
 
 class PokemonController {
+  find(req, res) {
+    const { id } = req.params;
+
+    db.query('SELECT * FROM manga WHERE id = $1', [id], function (error, results) {
+			if (error) throw error;
+			const manga = results.rows;
+
+			res.json({manga});
+		});
+  }
+
 	list(req, res) {
 		db.query('SELECT * FROM manga', function (error, results) {
 			if (error) throw error;
@@ -34,6 +45,23 @@ class PokemonController {
     db.query(
 			'DELETE FROM manga WHERE id = $1',
 			[id],
+			(error) => {
+        if (error) throw error;
+        res.redirect('/manga');
+      }
+		);
+  }
+
+  update(req, res) {
+    const { id } = req.params;
+    const { title, author, publish_year } = req.body;
+
+    const publish_year_date = new Date(publish_year);
+    publish_year_date.setTime(publish_year_date.getTime() + (1000 * 60 * 60 * 24))
+
+    db.query(
+			'UPDATE manga SET title = $1, author = $2, publish_year = $3 WHERE id = $4',
+			[title, author, publish_year_date, id],
 			(error) => {
         if (error) throw error;
         res.redirect('/manga');
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const routes = require('express').Router();
 const MangaController = require('./controller/MangaController');
 
 routes.get('/manga', MangaController.list);
+routes.get('/manga/:id', MangaController.find);
 routes.get('/manga/delete/:id', MangaController.remove);
 routes.post('/manga/add', MangaController.create);
+routes.post('/manga/update/:id', MangaController.update);
 
 module.exports = routes;
