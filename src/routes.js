1381038c06e4ce986f85c400e3e6e4dbb0a0612c
LuanSouzaSilveira <luansouzasilveira@gmail.com>
2020-08-18 12:54:26 -0300

    Register page

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ const MangaController = require('./controller/MangaController');
 const SessionController = require('./controller/SessionController');
 
 routes.get('/', SessionController.renderLanding);
-routes.get('/login', MangaController.list);
-routes.get('/register', MangaController.list);
+routes.get('/login', SessionController.renderLogin);
+routes.get('/register', SessionController.renderRegister);
 
 routes.get('/manga', MangaController.list);
 routes.get('/manga/:id', MangaController.find);
