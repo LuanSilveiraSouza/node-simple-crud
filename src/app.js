b9f8120b3197372370c0a21524d2bc6cee273320
LuanSouzaSilveira <luansouzasilveira@gmail.com>
2020-08-20 14:40:57 -0300

    Error 404 Page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
 const favicon = require('express-favicon');
 const path = require('path');
 
@@ -9,12 +9,16 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(routes);
+
+app.use('*', (req, res) => {
+	return res.render('error');
+});
 
 app.listen(process.env.PORT || 3000);
